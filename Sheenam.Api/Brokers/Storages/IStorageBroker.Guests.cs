20ff1e8e36d941f15c234d634fbb46181dc57787
xayotbek <hayotbek2525@gmail.com>
2024-05-25 16:17:52 +0500

    BROKERS: Insert Guest

---
+++ Sheenam.Api/Brokers/Storages/IStorageBroker.Guests.cs
@@ -0,0 +1,14 @@
+﻿using Sheenam.Api.Models.Foundations.Guests;
+using System.Threading.Tasks;
+
+namespace Sheenam.Api.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+        ValueTask<Guest> InsertGuestAsync(Guest guest);
+
+        //create
+        //read
+        //
+    }
+}
