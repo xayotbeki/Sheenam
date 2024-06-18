4e10a28d35112e3b4614e3b122ec858ced50a257
xayotbek <hayotbek2525@gmail.com>
2024-06-18 13:43:12 +0500

    BROKERS: Implement Logging

---
+++ Sheenam.Api/Brokers/Loggings/ILoggingBrokers.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Sheenam.Api.Brokers.Loggings
+{
+    public interface ILoggingBrokers
+    {
+        void LogError(Exception exception);
+        void LogCritical(Exception exception);
+    }
+}
---
+++ Sheenam.Api/Brokers/Loggings/LoggingBroker.cs
@@ -0,0 +1,30 @@
+﻿/*==============================
+ * Copyright(c) Coalition of Good-Hearted Engineers
+ * Free to Use Comfort and Peace
+ =================================*/
+
+
+
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace Sheenam.Api.Brokers.Loggings
+{
+    public class LoggingBroker : ILoggingBrokers
+    {
+        private readonly ILogger<LoggingBroker> logger;
+
+        public LoggingBroker(ILogger<LoggingBroker> logger) =>
+            this.logger = logger;
+
+        public void LogError(Exception exception) =>
+            this.logger.LogError(exception,exception.Message);
+        
+
+        public void LogCritical(Exception exception) =>
+            this.logger.LogCritical(exception,exception.Message);
+        
+        
+
+    }
+}
